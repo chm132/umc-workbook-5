3fe8cd62e2026e8b49f96bd70a1091283f4c695e
chm132 <147499337+chm132@users.noreply.github.com>
2023-11-26 22:54:25 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import Celebirity from "./pages/Celebirity";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import NotFound from "./pages/NotFound";
+import TV from "./pages/TV";
+import Header from "./Components/Header";
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+		  <Header></Header>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/TV" element={<TV/>} />
+            <Route path="/Celebirity" element={<Celebirity/>} />
+            <Route path="/Movies" element={<Movies/>} />
+            <Route path="/*" element={<NotFound />} />    
+            </Routes>
+     </BrowserRouter>
+  );
+}
+
+export default App;
